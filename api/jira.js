9dbe1b6ca1f4dad8afe0c5d55d3a6ce57895fefc
jbishop4837-debug <j.bishop4837@gmail.com>
2026-02-25 11:44:54 -0700

    Update jira.js

--- api/jira.js
+++ api/jira.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     const JIRA_EMAIL = "jbishop@enphaseenergy.com";
-    const JIRA_TOKEN = "YOUR_REAL_TOKEN_HERE";
+    const JIRA_TOKEN = "ATATT3xFfGF0X8-NEleRVd_C_9CXxuhKiczNYx53mfo6co1XM3Ko0RurjlTocgbgIdrQM_ymemA5klTME3hZE1j3SYtyj7v90QqgNi_7wFaDiOHHpp2YxqN2VuI-eMVjsDqshccqfXcfoGqFOj3d2UOCdFZGQc2TrEFtcxnfZmdurdCY-oCm6Lo=35B37B69";
     const JIRA_DOMAIN = "enphase.atlassian.net";
 
     const jql =
